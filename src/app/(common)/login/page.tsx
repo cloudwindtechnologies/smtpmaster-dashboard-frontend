9b4264d7a8f8f0478a0456570ce9f561b90837f1
AbakashCloudWind <abakash.cloudwind@gmail.com>
2026-04-06 16:00:55 +0530

    added date on the login field3

--- src/app/(common)/login/page.tsx
+++ src/app/(common)/login/page.tsx
@@ -182,7 +182,7 @@ function clearPendingRedirect() {
   return (
     
     <div className="h-screen w-screen overflow-hidden bg-[#f4f6fb] flex items-center justify-center">
-      date:06/04/2026
+      
       <div className="relative w-[70%] h-full transform scale-[0.8] origin-center">
         <div className="absolute inset-0 flex items-center justify-center">
           <div className="w-[1180px] h-[110vh] max-h-[900px]">
